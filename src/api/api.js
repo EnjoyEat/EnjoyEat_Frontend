3e85bc0df26381365dfb8ea0edbe6a7be4dd6eeb
shackstack <kms1061@naver.com>
2022-11-01 10:46:15 +0900

    [feat] 서버연결완료

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: `서버주소`,
+  baseURL: `http://3.37.215.170/`,
   headers: {
-    'content-type': 'application/json;charset=UTF-8',
-    accept: 'application/json,',
+    "content-type": "application/json;charset=UTF-8",
+    accept: "application/json,",
   },
-  withCredentials: true,
 });
 
 export const instances = {
-  // ex)
-  // getMainPage: async () => {
-  //   const response = await instance.get('/main');
-  //   return response.data;
-  // },
+  test: () => instance.get(`testing`),
 };
+// ex)
+// getMainPage: async () => {
+//   const response = await instance.get('/main');
+//   return response.data;
+// },
 
 // 여기서부터는 인터셉터를 다룹니다.
 
